9a7ef6b1dcf70182e8783edc1797335f97e3cd52
Fernando <fernando.lewandowski@gmail.com>
2016-08-31 15:17:50 -0300

    Update video_uploader.rb

--- app/uploaders/spina/video_uploader.rb
+++ app/uploaders/spina/video_uploader.rb
@@ -1,1 +1,23 @@
-
+# encoding: utf-8
+module Spina
+  class VideoUploader < CarrierWave::Uploader::Base
+    include CarrierWave::Video  # for your video processing
+    include CarrierWave::Video::Thumbnailer
+  
+    version :thumb do
+      process thumbnail: [{format: 'png', quality: 10, size: 192, strip: true, logger: Rails.logger}]
+      def full_filename for_file
+        png_name for_file, version_name
+      end
+    end
+  
+    def png_name for_file, version_name
+      %Q{#{version_name}_#{for_file.chomp(File.extname(for_file))}.png}
+    end
+    
+    def extension_white_list
+      %w(mp4)
+    end
+    
+  end
+end
